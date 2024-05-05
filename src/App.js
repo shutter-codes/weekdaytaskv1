6ecec6f39f531fcb050daf9b22944fc4a19511fe
Naitik Sharma <devnaitik104@gmail.com>
2024-05-05 18:17:17 +0530

    api configured

--- src/App.js
+++ src/App.js
@@ -1,63 +1,97 @@
-// App.js
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchJobs, setFilters } from './redux/actions';
-import JobCard from './components/JobCard';
-import FilterSection from './components/FilterSection';
-import './App.css';
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Grid, Box } from "@mui/material";
+import { useSelector } from "react-redux";
 
-const App = () => {
-  const dispatch = useDispatch();
-  const { jobs, filters, loading, hasMore } = useSelector((state) => state.jobs);
+// styles
+import "./App.css";
+// components
+import { Card, FilterGroup } from "./components";
+// utils & static data
+import { filteredJobs } from "./utils/helperFunctions";
+// api
+import { fetchJdList } from "./api/client";
 
-  useEffect(() => {
-    dispatch(fetchJobs());
+function App() {
+  const [data, setData] = useState([]);
 
-    const handleScroll = () => {
-      const scrollHeight = document.documentElement.scrollHeight;
-      const scrollTop = document.documentElement.scrollTop;
-      const clientHeight = document.documentElement.clientHeight;
+  const [allJobs, setAllJobs] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
 
-      if (scrollTop + clientHeight >= scrollHeight && hasMore && !loading) {
-        dispatch(fetchJobs());
+  const filters = useSelector((state) => state.filters);
+
+  const fetchJobList = async () => {
+    try {
+      const body = JSON.stringify({
+        limit: 24,
+        offset: offset,
+      });
+      const result = await fetchJdList(body);
+
+      if (result?.totalCount > 0) {
+        setData([...allJobs, result?.jdList]);
       }
-    };
+      setAllJobs((prevJobs) => [...prevJobs, ...result.jdList]);
+      setOffset((prevOffset) => prevOffset + 10);
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [dispatch, hasMore, loading]);
-
-  const handleFilterChange = (filterName, filterValue) => {
-    dispatch(setFilters({ ...filters, [filterName]: filterValue }));
+      if (result?.jdList.length === 0) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error("fetchJobList", error);
+    }
   };
 
-  const filteredJobs = jobs.filter((job) => {
-    return Object.keys(filters).every((filterName) => {
-      const filterValue = filters[filterName];
-      if (!filterValue) return true;
+  useEffect(() => {
+    fetchJobList();
+  }, []);
 
-      const jobValue = job[filterName];
-      if (!jobValue) return false;
-
-      if (Array.isArray(filterValue)) {
-        return filterValue.includes(jobValue);
-      } else {
-        return filterValue === jobValue;
-      }
-    });
-  });
+  useEffect(() => {
+    setData(filteredJobs(allJobs, filters));
+  }, [allJobs, filters]);
 
   return (
     <div className="App">
-      <FilterSection filters={filters} onFilterChange={handleFilterChange} />
-      <div className="job-cards">
-        {filteredJobs.map((job) => (
-          <JobCard key={job.id} job={job} />
-        ))}
-        {loading && <div>Loading...</div>}
-      </div>
+      <div>Filters</div>
+
+      <FilterGroup />
+
+      <InfiniteScroll
+        dataLength={data?.length}
+        next={fetchJobList}
+        hasMore={hasMore}
+        loader={
+          <Box display="flex" justifyContent="center" mt={2} minHeight="80px">
+            Fetching Available jobs...
+          </Box>
+        }
+        endMessage={
+          <Box textAlign="center" mt={2}>
+            <p>No more jobs to load</p>
+          </Box>
+        }
+        scrollThreshold={0.9}
+      >
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 2,
+          }}
+        >
+          {data?.map((job, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4} lg={3} xl={2}>
+              <Card key={job.jdUid} data={job} />
+            </Grid>
+          ))}
+        </Grid>
+      </InfiniteScroll>
     </div>
   );
-};
+}
 
 export default App;
