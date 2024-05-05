d356d0e9b6be62136efbb2b458bec3025c7d4f51
Naitik Sharma <devnaitik104@gmail.com>
2024-05-05 18:17:03 +0530

    api configured

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+ 
+  
+  export const fetchJdList = async (body) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+
+    };
+  
+
+    const res = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+    const result = await res?.json();
+    return result;
+  };
